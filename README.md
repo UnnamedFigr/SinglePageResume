558db034abde0f7955ecb3c7ac1b5df4b0638c6d
Ivan Drazhev <justdrazhev1@gmail.com>
2025-10-24 13:40:44 +0300

    Improved image format and added links to the buttons in script.js

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # SinglePageResume
-A website hosted through GitHub to present my resume.
+A website hosted through GitHub to present my resume. The website is created with simple HTML, CSS and JavaScript and hosted through GitHub.
+I will experiment with this website in the future adding interesting features.
--- scripts.js
+++ scripts.js
@@ -20,3 +20,18 @@ const links = [
     {'first-year':'https://github.com/UnnamedFigr/Technical-University-Sofia-Exercises/tree/main/First_year/SecondSemesterProject/CourseWork'} ,
     {'second-year': 'https://github.com/UnnamedFigr/Technical-University-Sofia-Exercises/tree/main/Second_year/Coursework_HtmlCrawler/HtmlCrawler/HtmlCrawler2.2'}
 ]
+const buttons = document.querySelectorAll(".project-link-button");
+buttons.forEach(btn => {
+    btn.addEventListener('click', () => {
+        const key = btn.getAttribute('data-project');
+        let url = null;
+        links.forEach(linkObj => {
+            if(linkObj.hasOwnProperty(key)){
+                url = linkObj[key];
+            }
+        })
+        if(url){
+            window.open(url, '_blank');
+        }
+    })
+})
